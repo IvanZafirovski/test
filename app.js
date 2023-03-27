177b6ad22a253c85301978b488c31bb94b8d5cbd
Ivan Zafirovski <ivan_zafirovski@yahoo.com>
2023-03-27 18:23:13 +0200

    testing

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-// const card = document.querySelector(".test");
+const card = document.querySelector(".test-one");
 
-// setInterval(() => {
-//   const textInput = document.querySelector(".input");
+setInterval(() => {
+  const textInput = document.querySelector(".input-one");
 
-//   card.style.backgroundColor = textInput.value;
-// }, 10);
+  card.style.backgroundColor = textInput.value;
+}, 10);
 
 const cardAll = document.querySelectorAll(".test");
 
 cardAll.forEach((card) => {
   setInterval(() => {
     const textInput = document.querySelector(".input");
+
+    card.style.backgroundColor = textInput.value;
+  }, 10);
+});
+
+const cardSecondRow = document.querySelectorAll(".test-two");
+
+cardSecondRow.forEach((card) => {
+  setInterval(() => {
+    const textInput = document.querySelector(".input-two");
 
     card.style.backgroundColor = textInput.value;
   }, 10);
