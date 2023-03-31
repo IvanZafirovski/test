326a891f20153e9d257742ac949313f2381b271e
Ivan Zafirovski <ivan_zafirovski@yahoo.com>
2023-03-31 04:28:02 +0200

    upddate

--- test/app.js
+++ test/app.js
@@ -1,3 +1,36 @@
+//Clock
+const currentTime = () => {
+  const el = document.querySelector("h1");
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  /// contition ? true, : false
+  hh = hh < 10 ? `${hh}` : hh;
+  mm = mm < 10 ? `${mm}` : mm;
+  ss = ss < 10 ? `${ss}` : ss;
+
+  let time = `${hh}:${mm}:${ss}`;
+  el.innerText = time;
+};
+currentTime();
+setInterval(currentTime, 1000);
+
+/// Dark Light
+
+const btns = document.getElementsByName("r");
+
+for (const btn of btns) {
+  btn.onclick = changeBg;
+}
+function changeBg() {
+  // const sectionRadio = document.querySelector(".section-radio");
+  // sectionRadio.style.background = this.value;
+  document.body.style.background = this.value;
+}
+
+// Changing color
 const card = document.querySelector(".test-one");
 
 setInterval(() => {
